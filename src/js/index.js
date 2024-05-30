0d58f0eb852e4fe447b55ad1cfb32e4553b21bbe
Pedro Ferraz <pepito.ferraz@gmail.com>
2024-05-30 18:20:37 -0300

    Conclusao da geracao de cursos em destaque

--- src/js/index.js
+++ src/js/index.js
@@ -1,65 +1,105 @@
-const URL = ``
 
+fetch('http://localhost:3009/cursoData', {
+    method: "GET",
+})
+    .then((res) => res.json())
+    .then((data) => {
 
+        window.onload = carroselElements
+        cursoDados = data.cursoData
+        qtdCursos = cursoDados.length
 
-//RESPONSIVIDADE - criando elementos no carrosel com base na largura da tela]
-
-function carroselElements(){
-
-    var larguraTela = window.innerWidth;
-    var carrosel = document.querySelectorAll('.carousel-item')
-
-    carrosel[0].innerHTML = '';
-    carrosel[1].innerHTML = '';
-    carrosel[2].innerHTML = '';
-    
-    var qtdElementos;
-
-    if(larguraTela > 1200){
-        qtdElementos = 4;
-    }
-    else if(larguraTela > 850){
-        qtdElementos = 3;
-    }
-    else if(larguraTela >576){
-        qtdElementos = 2;
-    }
-    else{
-        qtdElementos = 1;
-    }
-
-    var contador = 0;
-    carrosel.forEach((i) => {
-
-      var divContainer = document.createElement('div');
-
-      divContainer.classList.add('d-flex', 'gap-2', 'container-fluid', 'justify-content-center');
-
-        for (let i = 0; i<qtdElementos; i++){
-
-            var curso = document.createElement("div")
-            curso.style.border = "1px solid black"
-            curso.style.width = "fit-content"
-            if(larguraTela >1200){
-            curso.style.minWidth = "260px"
+        function gerarNumerosAleatorio(min, max, contador) {
+            if (max - min + 1 < contador) {
+                throw new Error("Intervalo pequeno demais para gerar a quantidade desejada de números.");
             }
-            else if(larguraTela > 850){
-                curso.style.minWidth = "240px"
-            }
-            else{
-                curso.style.minWidth = "220px"
-            }
-            curso.style.height = "150px"
 
-            divContainer.appendChild(curso)
+            const numerosUnicos = new Set();
+
+            while (numerosUnicos.size < contador) {
+                const randomNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+                numerosUnicos.add(randomNumber);
+            }
+
+            return Array.from(numerosUnicos);
         }
+
+         num = gerarNumerosAleatorio(0, qtdCursos -1, qtdCursos -1)
         
+        function carroselElements() {
 
-        carrosel[contador].appendChild(divContainer)
-        contador++;
-    });
+            var larguraTela = window.innerWidth;
+            var carrosel = document.querySelectorAll('.carousel-item')
 
-}
-window.onload = carroselElements
-window.onresize = carroselElements
+            carrosel[0].innerHTML = '';
+            carrosel[1].innerHTML = '';
+            carrosel[2].innerHTML = '';
+
+            var qtdElementos;
+
+            if (larguraTela > 1200) {
+                qtdElementos = 4;
+            }
+            else if (larguraTela > 850) {
+                qtdElementos = 3;
+            }
+            else if (larguraTela > 576) {
+                qtdElementos = 2;
+            }
+            else {
+                qtdElementos = 1;
+            }
+
+            var contador = 0;
+            carrosel.forEach((i) => {
+
+                var divContainer = document.createElement('div');
+
+                divContainer.classList.add('d-flex', 'gap-2', 'container-fluid', 'justify-content-center');
+
+                for (let i = 0; i < qtdElementos; i++) {
+
+                    var curso = document.createElement("div")
+
+                    curso.style.border = "2px solid black"
+                    curso.style.width = "fit-content"
+                    if (larguraTela > 1200) {
+                        curso.style.minWidth = "260px"
+                    }
+                    else if (larguraTela > 850) {
+                        curso.style.minWidth = "240px"
+                    }
+                    else {
+                        curso.style.minWidth = "220px"
+                    }
+                    curso.style.height = "150px"
+                    curso.style.borderRadius = "20px"
+                    curso.style.padding = "10px"
+                    curso.className = "cursoBox"
+
+                    curso.innerHTML = `         
+                <h5 class="text-primary">${cursoDados[num[i]].NomeCurso}</h5>
+                <p class="m-0 p-0"><strong>Área: </strong>${cursoDados[num[i]].AreaConhecimento}</p>
+                <p class="m-0 p-0"><strong>Carga Horaria: </strong>${cursoDados[num[i]].CargaHoraria} Horas</p>
+                <p class="m-0 p-0"><strong>Preço: </strong> R$${cursoDados[num[i]].Preco}</p>
+                <p class="m-0 p-0"><strong>Modalidade: </strong>${cursoDados[num[i]].Modalidade}</p>`
+
+                    divContainer.appendChild(curso)
+                }
+
+
+                carrosel[contador].appendChild(divContainer)
+                contador++;
+            });
+
+        }
+
+        window.onresize = carroselElements
+    })
+    .catch((err) => {
+        console.log("Erro ao buscar dados dos cursos")
+        console.log(err)
+    })
+
+
 
